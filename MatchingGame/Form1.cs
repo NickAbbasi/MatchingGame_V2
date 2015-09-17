60a27b99a9d7ac2cb698f8a1ebf116430ebcb568
NickAbbasi <nickabbasi1@gmail.com>
2015-09-16 21:02:03 -0400

    added comments

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -13,14 +13,14 @@ namespace MatchingGame
     public partial class Form1 : Form
     {
 
-        //creating random numbers
+
         Random random = new Random();
         List<string> icons = new List<string>()
         {
         "!", "!", "N", "N", ",", ",", "k", "k",
         "b", "b", "v", "v", "w", "w", "z", "z"
          };
-
+        //gets the the clicked on squares
         Label firstClicked = null;
         Label seccondClicked = null;
 
